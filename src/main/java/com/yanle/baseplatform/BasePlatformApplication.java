b2f795bc516c8ffec30df433e74ae4122c5bc607
yanlele <331393627@qq.com>
2019-08-28 22:56:55 +0800

    [+]init

---
+++ src/main/java/com/yanle/baseplatform/BasePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yanle.baseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasePlatformApplication.class, args);
+	}
+
+}
